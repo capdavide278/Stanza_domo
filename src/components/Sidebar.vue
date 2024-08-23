dddb495787e845cb32773c1ce6d0e53f3b183ca3
Davide Franco <franc.davi00@gmail.com>
2024-08-23 11:50:42 +0200

    aggiunta componenti

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,42 @@
+<!-- inserire il menù a sinistra con le icone che possono essere personalizzate -->
+
+<script>
+
+</script>
+
+<template>
+  <v-row>
+    <v-col
+      cols="12"
+    >
+      <v-card height="100%" width="10%">
+        <v-card-title class="bg-blue d-flex align-center">
+          <span class="text-h5">Menu</span>
+        </v-card-title>
+        
+        <v-spacer></v-spacer>
+
+        <!-- aggiungere ciclo vfor -->
+        <v-card-text>Lorem Ipsum</v-card-text>
+        <v-card-text>Lorem Ipsum</v-card-text>
+        <v-card-text>Lorem Ipsum</v-card-text>
+        <v-card-text>Lorem Ipsum</v-card-text>
+        <v-card-text>Lorem Ipsum</v-card-text>
+        <v-card-text>Lorem Ipsum</v-card-text>
+        <v-card-text>Lorem Ipsum</v-card-text>
+        <v-card-text>Lorem Ipsum</v-card-text>
+        <v-card-text>Lorem Ipsum</v-card-text>
+        <v-card-text>Lorem Ipsum</v-card-text>
+
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+
+<style scoped>
+    #sidebar{
+        width: 10%;
+        background-color: black;
+    }
+</style>
---
+++ src/components/stanze.vue
@@ -0,0 +1,4 @@
+<!-- prima parte carosello di stanze
+seconda parte viene riportato il nome della stanza
+terza parte rendere switch accesi o spenti
+-->
