3936504628a9ecdab06b71c2f3f972c09e5203be
Davide Franco <franc.davi00@gmail.com>
2024-08-23 16:51:01 +0200

    creazione footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="margin-top: 50px;display: grid;grid-template-columns: 5fr 5fr;">
+    <div style="margin-top: 50px;display: grid;grid-template-columns: 1fr 1fr;">
         <div>
             <v-card>
                 <v-card-title>
@@ -19,8 +19,15 @@
         <div>
             <v-card>
                 <v-card-title>
-                    Link
+                    Link ai software
                 </v-card-title>
+                <v-card-text style="margin-left: 8px;">
+                    <ul>
+                        <li><a href="https://vuejs.org/" style="text-decoration: none; color: black;">Vue 3.0</a></li>
+                        <li><a href="https://www.youtube.com/watch?v=gFxIfZ-u6cw&t=1971s" style="text-decoration: none; color: black;">Fantasia</a></li>
+                        <li a href="https://vuetifyjs.com/en/" style="text-decoration: none; color: black;">Vuetify</li>
+                    </ul>
+                </v-card-text>
             </v-card>
         </div>
     </div>
